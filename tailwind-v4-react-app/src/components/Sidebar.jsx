3581b2f049ab46bf291cebbe57d98f41a60105b8
Danny Ruchtie <46239581+DannyRuchtie@users.noreply.github.com>
2025-06-05 11:15:22 +0200

    tweaked styling

--- tailwind-v4-react-app/src/components/Sidebar.jsx
+++ tailwind-v4-react-app/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
   return (
-    <aside className="bg-white p-4 h-full border-l border-SG-stroke">
+    <aside className="bg-white p-4 h-full rounded-lg ">
        
     </aside>
   );
