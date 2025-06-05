7a4e8eb9bea4cd6233a586f7f57a15b183e831cf
Danny Ruchtie <46239581+DannyRuchtie@users.noreply.github.com>
2025-06-05 12:53:08 +0200

    small tweak to hover state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export default function Navbar({ setIsSlideOverOpen, setIsNotificationPanelOpen 
                         console.error('NAVBAR_ERROR: setIsSlideOverOpen is not a function when trying to open main menu');
                       }
                     }}
-                    className="p-2 rounded-md text-SG-text-muted hover:text-SG-text-primary hover:bg-SG-brand-neutral focus:outline-none focus:ring-2 focus:ring-inset focus:ring-SG-buttons-cta-primary"
+                    className="p-2 rounded-md text-SG-text-muted hover:text-SG-text-primary  hover:bg-SG-bg-active  focus:outline-none focus:ring-2 focus:ring-inset focus:ring-SG-buttons-cta-primary"
                   >
                     <span className="sr-only">Open sidebar</span>
                     <Bars3Icon className="h-6 w-6 text-SG-text-primary" aria-hidden="true" />
@@ -68,7 +68,7 @@ export default function Navbar({ setIsSlideOverOpen, setIsNotificationPanelOpen 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3 ">
                   <div>
-                    <MenuButton className="relative flex rounded-full bg-white text-sm  focus:ring-offset-2">
+                    <MenuButton className="relative flex rounded-full bg-white text-sm   focus:ring-offset-2">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
                       <img
@@ -102,7 +102,7 @@ export default function Navbar({ setIsSlideOverOpen, setIsNotificationPanelOpen 
               </div>
               <div className="-mr-2 flex items-center sm:hidden">
                 {/* Mobile menu button replaced with User Avatar */}
-                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-full bg-white p-1 text-gray-400 hover:bg-gray-100 hover:text-SG-text-muted focus:outline-none focus:ring-2 focus:ring-SG-brand-indigo focus:ring-offset-2">
+                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-full bg-white p-1 text-gray-400 hover:bg-SG-bg-active hover:text-SG-text-muted focus:outline-none focus:ring-2 focus:ring-SG-brand-indigo focus:ring-offset-2">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open user menu</span>
                   <img
