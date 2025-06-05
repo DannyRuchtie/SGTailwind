c5f13899c91e4b1e344deb64fe618fce12a3e0fc
Danny Ruchtie <46239581+DannyRuchtie@users.noreply.github.com>
2025-06-05 17:08:59 +0200

    tweaks to colors

--- src/components/layout/InfoPanel.jsx
+++ src/components/layout/InfoPanel.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export default function InfoPanel() {
   return (
     <aside className="bg-white p-4 h-full rounded-lg flex-col max-h-[calc(100vh-11rem)] min-h-[calc(100vh-11rem)]">
-      <h3 className="text-lg font-semibold text-SG-text-primary mb-3">Information Panel</h3>
+      <h3 className="text-lg font-semibold text-gray-900 mb-3">Information Panel</h3>
       <p className="text-sm text-SG-text-muted">This is the third column, the information panel.</p>
       {/* Additional content for the info panel can go here */}
     </aside>
